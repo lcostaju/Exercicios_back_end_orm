4a3139f201561a42bac652eeb84a2c7a60d9a00a
lcostaju <luciocj_eng@outlook.com>
2025-03-26 22:01:50 -0300

    Enhance HelloWordController to return ResponseEntity and add buscaPorId endpoint

--- hello_world/src/main/java/br/edu/iftm/tspi/pbackorm/hello_world/controller/HelloWordController.java
+++ hello_world/src/main/java/br/edu/iftm/tspi/pbackorm/hello_world/controller/HelloWordController.java
@@ -7,7 +7,9 @@ import br.edu.iftm.tspi.pbackorm.hello_world.domain.Contato;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
@@ -18,10 +20,32 @@ public class HelloWordController {
     public String hello() {
         return "Oi amigos";
     }
+    // @GetMapping
+    // public List<Contato> getContatos() {
+    //     return Arrays.asList(new Contato(1, "Goku"), 
+    //     new Contato(2, "Vegeta"));
+    // }
+
     @GetMapping
-    public List<Contato> getContatos() {
-        return Arrays.asList(new Contato(1, "Goku"), 
+    public ResponseEntity<List<Contato>> getContatos() {
+    List<Contato> contatos = Arrays.asList(new Contato(1, "Goku"), 
         new Contato(2, "Vegeta"));
+
+        return ResponseEntity.ok().body(contatos);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<List<Contato>> buscaPorId(@PathVariable Integer id) {
+
+        List<Contato> contatos = Arrays.asList(new Contato(1, "Goku"), 
+        new Contato(2, "Vegeta"));
+
+        for (Contato contato : contatos) {
+            if (contato.getCodigo().equals(id)) {
+                return ResponseEntity.ok().body(Arrays.asList(contato));
+            }
+        }
+        return ResponseEntity.notFound().build();
     }
     
 }
